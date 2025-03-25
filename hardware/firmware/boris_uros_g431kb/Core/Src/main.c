3985892595a519c4e65af049fbe9dae2c21a1529
keenan <keenanburke@outlook.com>
2025-03-25 18:41:35 -0400

    Got MG996 spinning from 0 to 180 degrees pretty precisely with timer 1

--- hardware/firmware/boris_uros_g431kb/Core/Src/main.c
+++ hardware/firmware/boris_uros_g431kb/Core/Src/main.c
@@ -129,6 +129,14 @@ int main(void)
   MX_TIM6_Init();
   /* USER CODE BEGIN 2 */
 
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+
+
+
+
+
+
+
   /* USER CODE END 2 */
 
   /* Init scheduler */
@@ -244,9 +252,9 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 79;
+  htim1.Init.Prescaler = 67;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 65535;
+  htim1.Init.Period = 50000;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
@@ -570,61 +578,75 @@ void * microros_zero_allocate(size_t number_of_elements, size_t size_of_element,
 void StartDefaultTask(void *argument)
 {
   /* USER CODE BEGIN 5 */
+
+	int i = 1250;
+
+	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, i);
+	vTaskDelay(pdMS_TO_TICKS(5000));
+
+	for(i; i <= 6200; i++)
+	{
+		__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, i);
+		vTaskDelay(pdMS_TO_TICKS(1));
+	}
+
+
+
   /* Infinite loop */
-	rmw_uros_set_custom_transport(
-	    true,
-	    (void *) &huart2,
-	    cubemx_transport_open,
-	    cubemx_transport_close,
-	    cubemx_transport_write,
-	    cubemx_transport_read);
-
-	  rcl_allocator_t freeRTOS_allocator = rcutils_get_zero_initialized_allocator();
-	  freeRTOS_allocator.allocate = microros_allocate;
-	  freeRTOS_allocator.deallocate = microros_deallocate;
-	  freeRTOS_allocator.reallocate = microros_reallocate;
-	  freeRTOS_allocator.zero_allocate =  microros_zero_allocate;
-
-	  if (!rcutils_set_default_allocator(&freeRTOS_allocator)) {
-	      printf("Error on default allocators (line %d)\n", __LINE__);
-	  }
-
-	  // micro-ROS app
-
-	  rcl_publisher_t publisher;
-	  std_msgs__msg__Int32 msg;
-	  rclc_support_t support;
-	  rcl_allocator_t allocator;
-	  rcl_node_t node;
-
-	  allocator = rcl_get_default_allocator();
-
-	  //create init_options
-	  rclc_support_init(&support, 0, NULL, &allocator);
-
-	  // create node
-	  rclc_node_init_default(&node, "cubemx_node", "", &support);
-
-	  // create publisher
-	  rclc_publisher_init_default(
-	    &publisher,
-	    &node,
-	    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-	    "cubemx_publisher");
-
-	  msg.data = 0;
-
-	  for(;;)
-	  {
-	    rcl_ret_t ret = rcl_publish(&publisher, &msg, NULL);
-	    if (ret != RCL_RET_OK)
-	    {
-	      printf("Error publishing (line %d)\n", __LINE__);
-	    }
-
-	    msg.data++;
-	    osDelay(10);
-	  }
+//	rmw_uros_set_custom_transport(
+//	    true,
+//	    (void *) &huart2,
+//	    cubemx_transport_open,
+//	    cubemx_transport_close,
+//	    cubemx_transport_write,
+//	    cubemx_transport_read);
+//
+//	  rcl_allocator_t freeRTOS_allocator = rcutils_get_zero_initialized_allocator();
+//	  freeRTOS_allocator.allocate = microros_allocate;
+//	  freeRTOS_allocator.deallocate = microros_deallocate;
+//	  freeRTOS_allocator.reallocate = microros_reallocate;
+//	  freeRTOS_allocator.zero_allocate =  microros_zero_allocate;
+//
+//	  if (!rcutils_set_default_allocator(&freeRTOS_allocator)) {
+//	      printf("Error on default allocators (line %d)\n", __LINE__);
+//	  }
+//
+//	  // micro-ROS app
+//
+//	  rcl_publisher_t publisher;
+//	  std_msgs__msg__Int32 msg;
+//	  rclc_support_t support;
+//	  rcl_allocator_t allocator;
+//	  rcl_node_t node;
+//
+//	  allocator = rcl_get_default_allocator();
+//
+//	  //create init_options
+//	  rclc_support_init(&support, 0, NULL, &allocator);
+//
+//	  // create node
+//	  rclc_node_init_default(&node, "cubemx_node", "", &support);
+//
+//	  // create publisher
+//	  rclc_publisher_init_default(
+//	    &publisher,
+//	    &node,
+//	    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+//	    "cubemx_publisher");
+//
+//	  msg.data = 0;
+//
+//	  for(;;)
+//	  {
+//	    rcl_ret_t ret = rcl_publish(&publisher, &msg, NULL);
+//	    if (ret != RCL_RET_OK)
+//	    {
+//	      printf("Error publishing (line %d)\n", __LINE__);
+//	    }
+//
+//	    msg.data++;
+//	    osDelay(10);
+//	  }
   /* USER CODE END 5 */
 }
 
